6b1317f1dd06448cd13e2624b949b1f9b1f6ff86
Patricia <Patricia@Patricias-MacBook-Air.local>
2017-07-07 10:06:05 -0700

    First spec passing

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,6 +4,7 @@ describe Person do
   it "requires a name to be valid" do
     # Write your spec here.
     # You can delete the pending line.
-    pending "Not written yet"
+    pat = Person.new(first_name: "Patricia")
+    expect(pat.first_name).to_not be_nil
   end
 end
